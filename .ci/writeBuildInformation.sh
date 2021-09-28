e2044a4369399220214eb55bfa7ef80fb27daa20
jkoenig <julian.koenig@js-soft.com>
2021-09-28 12:52:44 +0200

    chore: BITBUCKET_BUILD_NUMBER -> BUILD_NUMBER

--- .ci/writeBuildInformation.sh
+++ .ci/writeBuildInformation.sh
@@ -7,8 +7,10 @@ then
     exit 1
 fi
 
-sed -i "s~{{dependencies}}~$(jq .dependencies package.json -cr)~" ./dist/RuntimeBuildInformation.js
-sed -i "s/{{version}}/$(jq .version package.json -cr)/" ./dist/RuntimeBuildInformation.js
-sed -i "s/{{build}}/$BITBUCKET_BUILD_NUMBER/" ./dist/RuntimeBuildInformation.js
-sed -i "s/{{commit}}/$(git rev-parse HEAD)/" ./dist/RuntimeBuildInformation.js
-sed -i "s/{{date}}/$(date -u --iso-8601=seconds)/" ./dist/RuntimeBuildInformation.js
+TARGET_FILE="./dist/RuntimeBuildInformation.js"
+
+sed -i "s~{{dependencies}}~$(jq .dependencies package.json -cr)~" $TARGET_FILE
+sed -i "s/{{version}}/$(jq .version package.json -cr)/" $TARGET_FILE
+sed -i "s/{{build}}/$BUILD_NUMBER/" $TARGET_FILE
+sed -i "s/{{commit}}/$COMMIT_HASH/" $TARGET_FILE
+sed -i "s/{{date}}/$(date -u --iso-8601=seconds)/" $TARGET_FILE
