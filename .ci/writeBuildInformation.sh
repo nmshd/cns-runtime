30dd7a8df8d4f376cadc0eb6e7490a654e968b24
Timo Notheisen <timo.notheisen@js-soft.com>
2021-09-29 08:14:32 +0200

    fix: remove old content of writeBuildInformation

--- .ci/writeBuildInformation.sh
+++ .ci/writeBuildInformation.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-if ! command -v jq &>/dev/null; then
-    echo "jq could not be found"
-    exit 1
-fi
-
-TARGET_FILE="./dist/RuntimeBuildInformation.js"
-
-sed -i "s~{{dependencies}}~$(jq .dependencies package.json -cr)~" $TARGET_FILE
-sed -i "s/{{version}}/$(jq .version package.json -cr)/" $TARGET_FILE
-sed -i "s/{{build}}/$BUILD_NUMBER/" $TARGET_FILE
-sed -i "s/{{commit}}/$COMMIT_HASH/" $TARGET_FILE
-sed -i "s/{{date}}/$(date -u --iso-8601=seconds)/" $TARGET_FILE
-#!/usr/bin/env bash
-set -e
-
 if [ -z "$(which jq)" ]; then
     echo "jq could not be found"
     exit 1
