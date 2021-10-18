4f242c4e6dc29e451d8ed8024740d76af784681c
jkoenig <julian.koenig@js-soft.com>
2021-10-18 08:02:39 +0200

    chore: add comment for audit with higher audit-level

--- .ci/build.sh
+++ .ci/build.sh
@@ -5,5 +5,9 @@ npm ci
 npm run lint:prettier
 npm run lint:eslint
 npx license-check
+
+# dependencies should only be checked with audit level 'high'
+# because there is currently an issue with one of the dependencies (qrcode)
 npm audit --audit-level high
+
 npm run build:ci
