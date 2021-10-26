d9121ddd4e7f01048101daefec3bfa26b097f496
Samuel Melm <samuel.melm@js-soft.com>
2021-10-26 13:07:49 +0200

    feat(files): validate GetFileRequest with json schema

--- src/useCases/transport/files/GetFile.ts
+++ src/useCases/transport/files/GetFile.ts
@@ -1,24 +1,20 @@
 import { Result } from "@js-soft/ts-utils";
-import { BackboneIds, CoreId, File, FileController } from "@nmshd/transport";
+import { CoreId, File, FileController } from "@nmshd/transport";
 import { Inject } from "typescript-ioc";
 import { FileDTO } from "../../../types";
-import { IdValidator, RuntimeErrors, RuntimeValidator, UseCase } from "../../common";
+import { RuntimeErrors, UseCase } from "../../common";
+import { SchemaRepository } from "../../common/SchemaRepository";
+import { SchemaValidator } from "../../common/SchemaValidator";
 import { FileMapper } from "./FileMapper";
 import { GetFileRequest } from "./requests/GetFileRequest";
 
 export { GetFileRequest };
 
-class GetFileRequestValidator extends RuntimeValidator<GetFileRequest> {
-    public constructor() {
-        super();
-
-        this.validateIf((x) => x.id).fulfills(IdValidator.required(BackboneIds.file));
-    }
-}
+// this.validateIf((x) => x.id).fulfills(IdValidator.required(BackboneIds.file));
 
 export class GetFileUseCase extends UseCase<GetFileRequest, FileDTO> {
-    public constructor(@Inject private readonly fileController: FileController, @Inject validator: GetFileRequestValidator) {
-        super(validator);
+    public constructor(@Inject private readonly fileController: FileController, @Inject schemas: SchemaRepository) {
+        super(new SchemaValidator(schemas, "GetFileRequest"));
     }
 
     protected async executeInternal(request: GetFileRequest): Promise<Result<FileDTO>> {
