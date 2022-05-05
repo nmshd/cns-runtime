1f1cdb62cfa21e534478a80ac0ed422c6ead718b
tnotheis <timo.notheisen@js-soft.com>
2022-05-05 14:37:03 +0200

    chore: PR comments regarding events

--- src/events/consumption/IncomingRequestStatusChangedEvent.ts
+++ src/events/consumption/IncomingRequestStatusChangedEvent.ts
@@ -13,5 +13,7 @@ export class IncomingRequestStatusChangedEvent extends DataEvent<IncomingRequest
 
     public constructor(eventTargetAddress: string, data: IncomingRequestStatusChangedEventData) {
         super(IncomingRequestStatusChangedEvent.namespace, eventTargetAddress, data);
+
+        if (data.request.isOwn) throw new Error("Cannot create this event for an outgoing Request");
     }
 }
