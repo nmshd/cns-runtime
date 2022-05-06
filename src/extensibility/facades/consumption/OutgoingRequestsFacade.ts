a29cda33b09af4b10e561fea8836961d9a0c80da
tnotheis <timo.notheisen@js-soft.com>
2022-05-06 08:33:08 +0200

    refactor: rename properties in OutgoingRequestsFacade

--- src/extensibility/facades/consumption/OutgoingRequestsFacade.ts
+++ src/extensibility/facades/consumption/OutgoingRequestsFacade.ts
@@ -19,42 +19,42 @@ import {
 
 export class OutgoingRequestsFacade {
     public constructor(
-        @Inject private readonly canCreateOutgoingRequests: CanCreateOutgoingRequestUseCase,
-        @Inject private readonly createOutgoingRequests: CreateOutgoingRequestUseCase,
-        @Inject private readonly sentOutgoingRequests: SentOutgoingRequestUseCase,
-        @Inject private readonly createAndCompleteOutgoingRequestFromRelationshipCreationChange: CreateAndCompleteOutgoingRequestFromRelationshipCreationChangeUseCase,
-        @Inject private readonly completeOutgoingRequests: CompleteOutgoingRequestUseCase,
-        @Inject private readonly getOutgoingRequest: GetOutgoingRequestUseCase,
-        @Inject private readonly getOutgoingRequests: GetOutgoingRequestsUseCase
+        @Inject private readonly canCreateUseCase: CanCreateOutgoingRequestUseCase,
+        @Inject private readonly createUseCase: CreateOutgoingRequestUseCase,
+        @Inject private readonly sentUseCase: SentOutgoingRequestUseCase,
+        @Inject private readonly createAndCompleteFromRelationshipCreationChangeUseCase: CreateAndCompleteOutgoingRequestFromRelationshipCreationChangeUseCase,
+        @Inject private readonly completeUseCase: CompleteOutgoingRequestUseCase,
+        @Inject private readonly getRequestUseCase: GetOutgoingRequestUseCase,
+        @Inject private readonly getRequestsUseCase: GetOutgoingRequestsUseCase
     ) {}
 
     public async canCreate(request: CreateOutgoingRequestRequest): Promise<Result<RequestValidationResultDTO>> {
-        return await this.canCreateOutgoingRequests.execute(request);
+        return await this.canCreateUseCase.execute(request);
     }
 
     public async create(request: CreateOutgoingRequestRequest): Promise<Result<ConsumptionRequestDTO>> {
-        return await this.createOutgoingRequests.execute(request);
+        return await this.createUseCase.execute(request);
     }
 
     public async createAndCompleteFromRelationshipCreationChange(
         request: CreateAndCompleteOutgoingRequestFromRelationshipCreationChangeRequest
     ): Promise<Result<ConsumptionRequestDTO>> {
-        return await this.createAndCompleteOutgoingRequestFromRelationshipCreationChange.execute(request);
+        return await this.createAndCompleteFromRelationshipCreationChangeUseCase.execute(request);
     }
 
     public async sent(request: SentOutgoingRequestRequest): Promise<Result<ConsumptionRequestDTO>> {
-        return await this.sentOutgoingRequests.execute(request);
+        return await this.sentUseCase.execute(request);
     }
 
     public async complete(request: CompleteOutgoingRequestRequest): Promise<Result<ConsumptionRequestDTO>> {
-        return await this.completeOutgoingRequests.execute(request);
+        return await this.completeUseCase.execute(request);
     }
 
     public async getRequest(request: GetOutgoingRequestRequest): Promise<Result<ConsumptionRequestDTO>> {
-        return await this.getOutgoingRequest.execute(request);
+        return await this.getRequestUseCase.execute(request);
     }
 
     public async getRequests(request: GetOutgoingRequestsRequest): Promise<Result<ConsumptionRequestDTO[]>> {
-        return await this.getOutgoingRequests.execute(request);
+        return await this.getRequestsUseCase.execute(request);
     }
 }
