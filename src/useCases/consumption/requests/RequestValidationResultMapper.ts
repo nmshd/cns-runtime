78707ba7727d253529e7d87ea04c46ec30471b3c
Stnmtz <t.stnmtz@outlook.de>
2022-05-30 10:00:50 +0200

    fix: ValidationResultMapper

--- src/useCases/consumption/requests/RequestValidationResultMapper.ts
+++ src/useCases/consumption/requests/RequestValidationResultMapper.ts
@@ -1,12 +1,12 @@
-import { ValidationResult } from "@nmshd/consumption";
+import { ErrorValidationResult, ValidationResult } from "@nmshd/consumption";
 import { RequestValidationResultDTO } from "../../../types";
 
 export class RequestValidationResultMapper {
     public static toRequestValidationResultDTO(request: ValidationResult): RequestValidationResultDTO {
         return {
             isSuccess: request.isSuccess(),
-            code: request.isError() ? request.code : undefined,
-            message: request.isError() ? request.message : undefined,
+            code: request.isError() ? (request as ErrorValidationResult).error.code : undefined,
+            message: request.isError() ? (request as ErrorValidationResult).error.message : undefined,
             items: request.items.map((item) => this.toRequestValidationResultDTO(item))
         };
     }
