3b5fcfd05b33292b1b83608656ff642fc8024523
Stnmtz <t.stnmtz@outlook.de>
2022-05-30 12:11:23 +0200

    test: enable dvo tests

--- test/dataViews/relationshipDVO.test.ts
+++ test/dataViews/relationshipDVO.test.ts
@@ -1,5 +1,7 @@
-import { DataViewExpander, TransportServices } from "@nmshd/runtime";
-import { RuntimeServiceProvider } from "../lib";
+import { GivenName, IdentityAttribute, ReadAttributeAcceptResponseItem, ReadAttributeRequestItem, ResponseItemResult, ResponseResult } from "@nmshd/content";
+import { CoreAddress, CoreId } from "@nmshd/transport";
+import { DataViewExpander, TransportServices } from "../../src";
+import { establishRelationshipWithBodys, RuntimeServiceProvider } from "../lib";
 
 const serviceProvider = new RuntimeServiceProvider();
 let transportServices1: TransportServices;
@@ -15,29 +17,45 @@ beforeAll(async () => {
     expander2 = runtimeServices[1].expander;
     await establishRelationshipWithBodys(
         transportServices1,
-        transportServices2
+        transportServices2,
         {
-            "@type": "RelationshipTemplateBody",
-            sharedAttributes: [
-                { "@type": "Attribute", name: "Person.givenName", value: "Jürgen" },
-                { "@type": "Attribute", name: "Person.familyName", value: "Becker" }
-            ]
+            onNewRelationship: {
+                "@type": "Request",
+                items: [
+                    ReadAttributeRequestItem.from({
+                        mustBeAccepted: true,
+                        query: {
+                            valueType: "GivenName"
+                        }
+                    })
+                ]
+            }
         },
         {
-            "@type": "RelationshipCreationChangeRequestBody",
-            sharedAttributes: [
-                { "@type": "Attribute", name: "Person.gender", value: "f" },
-                { "@type": "Attribute", name: "Person.familyName", value: "Sèzanné" }
-            ]
+            response: {
+                "@type": "Response",
+                result: ResponseResult.Accepted,
+                requestId: await CoreId.generate(),
+                items: [
+                    ReadAttributeAcceptResponseItem.from({
+                        result: ResponseItemResult.Accepted,
+                        attributeId: await CoreId.generate(),
+                        attribute: IdentityAttribute.from({
+                            owner: CoreAddress.from((await transportServices1.account.getIdentityInfo()).value.address),
+                            value: GivenName.fromAny({
+                                value: "AGivenName"
+                            })
+                        })
+                    })
+                ]
+            }
         }
     );
 }, 30000);
 
 afterAll(() => serviceProvider.stop());
 
-// TODO: re-enable when DVOs work again
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip("RelationshipDVO", () => {
+describe("RelationshipDVO", () => {
     test("check the relationship dvo for the templator", async () => {
         const dtos = (await transportServices1.relationships.getRelationships({})).value;
         const dvos = await expander1.expandRelationshipDTOs(dtos);
@@ -45,8 +63,8 @@ describe.skip("RelationshipDVO", () => {
         const dvo = dvos[0];
         expect(dvo).toBeDefined();
         expect(dvo.id).toBe(dto.peer);
-        expect(dvo.name).toBe("i18n://salutation.gender.f Sèzanné");
-        expect(dvo.description).toBe("i18n://dvo.relationship.Active");
+        expect(dvo.name).toBe("");
+        expect(dvo.description).toBe(""); // i18n://dvo.relationship.Active
         expect(dvo.type).toBe("IdentityDVO");
         expect(dvo.date).toBe(dto.changes[0].request.createdAt);
         expect(dvo.isSelf).toBe(false);
@@ -80,8 +98,8 @@ describe.skip("RelationshipDVO", () => {
         const dvo = dvos[0];
         expect(dvo).toBeDefined();
         expect(dvo.id).toBe(dto.peer);
-        expect(dvo.name).toBe("Jürgen Becker");
-        expect(dvo.description).toBe("i18n://dvo.relationship.Active");
+        expect(dvo.name).toBe("");
+        expect(dvo.description).toBe(""); // i18n://dvo.relationship.Active
         expect(dvo.type).toBe("IdentityDVO");
         expect(dvo.date).toBe(dto.changes[0].request.createdAt);
         expect(dvo.isSelf).toBe(false);
