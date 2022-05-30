3786e24b640c5a0c093b8975b872dee9fe902d7b
Stnmtz <t.stnmtz@outlook.de>
2022-05-30 10:07:27 +0200

    fix: remove assertion

--- src/useCases/consumption/requests/RequestValidationResultMapper.ts
+++ src/useCases/consumption/requests/RequestValidationResultMapper.ts
@@ -1,12 +1,12 @@
-import { ErrorValidationResult, ValidationResult } from "@nmshd/consumption";
+import { ValidationResult } from "@nmshd/consumption";
 import { RequestValidationResultDTO } from "../../../types";
 
 export class RequestValidationResultMapper {
     public static toRequestValidationResultDTO(request: ValidationResult): RequestValidationResultDTO {
         return {
             isSuccess: request.isSuccess(),
-            code: request.isError() ? (request as ErrorValidationResult).error.code : undefined,
-            message: request.isError() ? (request as ErrorValidationResult).error.message : undefined,
+            code: request.isError() ? request.error.code : undefined,
+            message: request.isError() ? request.error.message : undefined,
             items: request.items.map((item) => this.toRequestValidationResultDTO(item))
         };
     }
